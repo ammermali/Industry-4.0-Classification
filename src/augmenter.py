f567d3a24f95501023d5506ff6df2c2241a42677
ammermali <115208836+ammermali@users.noreply.github.com>
2026-03-02 22:26:35 +0100

    Augmenter
    Added a draft for a modular component to add a Data Augmentation Layer in the Preprocessing Phase.

---
+++ src/augmenter.py
@@ -0,0 +1,16 @@
+import tensorflow as tf
+from tensorflow.keras import layers
+
+def get_augmenter():
+    return tf.keras.Sequential([
+        layers.Rescaling(1./255),
+        layers.RandomFlip("horizontal_and_vertical"),
+        layers.RandomRotation(0.1, fill_mode="nearest"),
+        layers.RandomZoom(0.05, fill_mode="nearest")
+    ])
+
+def apply_augmentation(dataset, augmenter):
+    return dataset.map(
+        lambda x,y: (augmenter(x,training=True), y),
+        num_parallel_calls=tf.data.AUTOTUNE
+    )
