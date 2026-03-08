5a8d3a1fefae63e52eea63081ea5e3ace405269b
ammermali <115208836+ammermali@users.noreply.github.com>
2026-03-08 18:40:45 +0100

    New augmenter
    Changed the augmenter logic so that it adds additional data instead of replacing them (in the training phase).

--- src/data_augmenter.py
+++ src/data_augmenter.py
@@ -1,15 +1,19 @@
 import tensorflow as tf
 from tensorflow.keras import layers
 
+# Component that defines and applies the augmentation.
+
 def get_augmenter():
     return tf.keras.Sequential([
         layers.RandomFlip("horizontal_and_vertical"),
         layers.RandomRotation(0.2, fill_mode="nearest"),
-        layers.RandomZoom(0.2, fill_mode="nearest")
+        layers.RandomZoom(0.2, fill_mode="nearest"),
+        layers.RandomContrast(factor=0.2)
     ])
 
 def apply_augmentation(dataset, augmenter):
-    return dataset.map(
-        lambda x,y: (augmenter(x,training=True), y),
+    augmented_ds = dataset.map(
+        lambda x, y: (augmenter(x,training=True), y),
         num_parallel_calls=tf.data.AUTOTUNE
     )
+    return dataset.concatenate(augmented_ds)
